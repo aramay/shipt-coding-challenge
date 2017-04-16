f8e72e89bc33863bb985bd51aa2b20d83bde36fc
“kasper341” <kasper_341@hotmail.com>
2017-04-16 15:53:14 -0700

    add specs for user-login use case

--- spec.js
+++ spec.js
@@ -2,7 +2,7 @@
 
 var IndexPage = require('./IndexPage')
 
-describe('Protractor Demo App', function() {
+describe('Shipt coding challenge', function() {
 
   var page = new IndexPage();
 
@@ -11,40 +11,64 @@ describe('Protractor Demo App', function() {
     page.get()
   })
 
-  it("should have a title", function(){
+  xit("should have a title", function(){
     // expect(browser.getTitle()).toEqual('Super Calculator');
 
     //use page object to interact with page
-    expect(page.getTitle()).toEqual('Super Calculator');
+    expect(page.getTitle()).toEqual('Shipt Groceries');
+  })
+
+  xit("user should be able to login", function(){
+
+    var email = element(by.model('login.username')).sendKeys("qatest@shipt.com")
+    var password = element(by.model('login.password')).sendKeys("Sh1pt123!")
+
+    element(by.id('start_shopping_login_button')).click()
+
+    expect(element(by.css('[ng-click="viewModel.accountClick()"]')).isPresent()).toBe(true);
+
+  })
+
+  it("user should be able to search items with search feature", function(){
+
+    var email = element(by.model('login.username')).sendKeys("qatest@shipt.com")
+    var password = element(by.model('login.password')).sendKeys("Sh1pt123!")
+
+    element(by.id('start_shopping_login_button')).click()
+
+    element(by.model("search.searchQuery")).sendKeys("plum orga")
+
+    element.all(by.repeater('product in searchResults'))
+     
   })
   
   
-  it('should have add 2 and 2', function() {
+  // it('should have add 2 and 2', function() {
     
 
-    // console.log(page.getResult())
-    page.add(2,2)
-    expect(page.getResult()).toEqual('4');
+  //   page.add(2,2)
+  //   expect(page.getResult()).toEqual('4');
+  //   // console.log(page.getResult())
 
-    // element(by.model('first')).sendKeys(2)
-    // element(by.model('second')).sendKeys(2)
-    // element(by.id('gobutton')).click()
-    // firstNum.sendKeys(2)
-    // secondNum.sendKeys(2)
-    // button.click()
+  //   // element(by.model('first')).sendKeys(2)
+  //   // element(by.model('second')).sendKeys(2)
+  //   // element(by.id('gobutton')).click()
+  //   // firstNum.sendKeys(2)
+  //   // secondNum.sendKeys(2)
+  //   // button.click()
     // expect(browser.getTitle()).toEqual('Super calculator');
 
-  })
+  // })
   
-  it('should have a history', function(){
+  // it('should have a history', function(){
 
-    page.add(1,2)
-    expect(page.getHistory()).toEqual(1)
-    // add(1, 2)
-    // expect(history.count()).toEqual(1)
-    // add(3, 2)
-    // expect(history.count()).toEqual(2)
+  //   page.add(1,2)
+  //   expect(page.getHistory()).toEqual(1)
+  //   // add(1, 2)
+  //   // expect(history.count()).toEqual(1)
+  //   // add(3, 2)
+  //   // expect(history.count()).toEqual(2)
 
 
-  })
+  // })
 });
