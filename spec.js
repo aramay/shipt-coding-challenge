134729c0dfcefc15c2d9ac2189351a9aeaa213d0
“kasper341” <kasper_341@hotmail.com>
2017-04-16 21:54:12 -0700

    fix broken tests

--- spec.js
+++ spec.js
@@ -7,18 +7,16 @@ describe('Shipt coding challenge', function() {
   var page = new IndexPage();
 
   beforeEach(function(){
-    // browser.get('http://juliemr.github.io/protractor-demo/');
     page.get()
   })
 
-  xit("should have a title", function(){
-    // expect(browser.getTitle()).toEqual('Super Calculator');
+  it("should have a page title", function(){
 
     //use page object to interact with page
     expect(page.getTitle()).toEqual('Shipt Groceries');
   })
 
-  xit("user should be able to login", function(){
+  it("user should be able to login", function(){
 
     page.login()
 
@@ -26,22 +24,13 @@ describe('Shipt coding challenge', function() {
 
   })
 
-  xit("user should be able to search items with search feature", function(){
-
-    var email = element(by.model('login.username')).sendKeys("qatest@shipt.com")
-    var password = element(by.model('login.password')).sendKeys("Sh1pt123!")
-
-    element(by.id('start_shopping_login_button')).click()
+  it("user should be able to search items with search feature", function(){
 
     //search for item
-    element(by.model("search.searchQuery")).sendKeys("plum orga")
+    page.searchItem("plum orga")
 
     //get item text(title)
-    var itemText = element.all(by.repeater('product in searchResults')).get(0).getText()
-
-    // browser.wait(EC.presenceOf(elm), 10000);
-    
-    // var item = element.all(by.repeater('product in searchResults')).get(0).click()
+    var itemText = page.getProductNameSearchResult()
 
     //find first item in search
     var firstItem = element.all(by.repeater('product in searchResults')).first()
@@ -50,7 +39,7 @@ describe('Shipt coding challenge', function() {
     firstItem.element(by.css('[ng-click="addItem(product)"]')).click()
 
     //click cart link
-    element(by.css('[ng-click="cartClick()"]')).click()
+    page.clickCartLink()
 
     //get text of item in cart
     var itemInCart = element(by.css('[ng-click="clickCartItem(cartItem)"]')).getText()
@@ -62,8 +51,6 @@ describe('Shipt coding challenge', function() {
 
   it("user should be able to search items with category menu", function(){
 
-    //login user
-    page.login()
     //click menu tabs
     page.getMenuTabLink()
     //click item in category
@@ -76,10 +63,8 @@ describe('Shipt coding challenge', function() {
 
     //add item to cart
     productInMenuCategory.element(by.css('[ng-click="addItem(product)"]')).click()
-    // page.addItemToCart(productInMenuCategory)
 
     //click cart link
-    // element(by.css('[ng-click="cartClick()"]')).click()
     page.clickCartLink()
 
     //get text of item in cart
