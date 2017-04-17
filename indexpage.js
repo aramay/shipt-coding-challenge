3505c3270b0c69a9fce1aa084ad58fa848180de3
“kasper341” <kasper_341@hotmail.com>
2017-04-16 19:28:15 -0700

    add PageObject model - refactored login function

--- indexpage.js
+++ indexpage.js
@@ -1,40 +1,51 @@
-function IndexPage(){
+function IndexPage () {
 
-  this.firstNum = element(by.model('first'))
-  this.secondNum = element(by.model('second'))
-  this.button = element(by.id('gobutton'))
+  this.email = element(by.model('login.username'))
+    
+  this.password = element(by.model('login.password'))
+
+  this.loginButton = element(by.id('start_shopping_login_button'))
+
   this.result = element(by.binding('latest'))
   this.history = element.all(by.repeater('result in memory'))
 
+  this.get = function () {
+    // browser.get('https://www.shipt.com/main/')
+    browser.get('https://shop.shipt.com/#/app/home')
+  }
 
-  this.get = function(){
-    browser.get('http://juliemr.github.io/protractor-demo/');
-  };
+  this.login = function(){
+    this.get()
+    this.email.sendKeys("qatest@shipt.com")
+    this.password.sendKeys("Sh1pt123!")
 
-  this.getTitle = function(){
+    this.loginButton.click()
+
+  }
+
+  this.getTitle = function () {
     return browser.getTitle()
-  };
+  }
 
-  this.clickButton = function(){
+  this.clickButton = function () {
     this.button.click()
-  };
+  }
 
-  this.add = function(a, b){
+  this.add = function (a, b) {
     this.firstNum.sendKeys(a)
     this.secondNum.sendKeys(b)
     // button.click()
     this.clickButton()
-  };
-
-  this.getResult = function(){
-    // console.log("result ", this.result)
-    return this.result.getText()
-  };
-
-  this.getHistory = function(){
-    return this.history.count()
   }
 
+  this.getResult = function () {
+    // console.log("result ", this.result)
+    return this.result.getText()
+  }
+
+  this.getHistory = function () {
+    return this.history.count()
+  }
 }
 
-module.exports = IndexPage;
+module.exports = IndexPage
